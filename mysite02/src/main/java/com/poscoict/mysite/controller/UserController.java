de83d3706da2be85f6606a010599caf4af7abe41
Bella <shdudtnr3939@naver.com>
2022-01-14 15:39:51 +0900

    22.01.14 mysite2 init 2(MainAction,MainCont,Action..)

--- mysite02/src/main/java/com/poscoict/mysite/controller/UserController.java
+++ mysite02/src/main/java/com/poscoict/mysite/controller/UserController.java
@@ -1,39 +1,31 @@
 package com.poscoict.mysite.controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class UserController
- */
+import com.poscoict.mysite.mvc.main.MainActionFactory;
+import com.poscoict.web.mvc.Action;
+import com.poscoict.web.mvc.ActionFactory;
+
 public class UserController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("utf-8");
 		
 		String actionName = request.getParameter("a");
-		if("joinform".equals(actionName)){
-			
-		}else if("join".equals(actionName)) {
-			
-		// 이상한 요청 들어왔을 때	
-		}else {
-			
-		}
+		
+		ActionFactory af = new MainActionFactory();
+		Action action = new MainActionFactory().getAction(actionName);
+		action.execute(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
 		doGet(request, response);
 	}
 
