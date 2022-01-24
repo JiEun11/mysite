d8e49953ee5b7b2be00b0dd8b157635dda905e64
Bella <shdudtnr3939@naver.com>
2022-01-25 08:39:34 +0900

    22.01.25 webUtil & userRepo 작업

--- mysite03-1/src/main/java/com/poscoict/mysite/repository/UserRepository.java
+++ mysite03-1/src/main/java/com/poscoict/mysite/repository/UserRepository.java
@@ -33,12 +33,62 @@ public class UserRepository {
 		return conn;
 	}
 	
+	public boolean updateUser(UserVo userVo) {
+		boolean result = false;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+			conn = getConnection();
+			String sql = null;
+			
+			//3. SQL준비
+			if(userVo.getPassword() == null && "".equals(userVo.getPassword())) {
+				sql = "update user set name=?, gender=? where no=?";
+				pstmt = conn.prepareStatement(sql);
+
+				// 4. 바인딩(binding)
+				pstmt.setString(1, userVo.getName());
+				pstmt.setString(2, userVo.getGender());
+				pstmt.setLong(3, userVo.getNo());
+			
+			}else {
+				sql = "update user set name=?, gender=?, password=? where no=?";
+				pstmt = conn.prepareStatement(sql);
+				
+				// 4. 바인딩(binding)
+				pstmt.setString(1, userVo.getName());
+				pstmt.setString(2, userVo.getGender());
+				pstmt.setString(3, userVo.getPassword());
+				pstmt.setLong(4, userVo.getNo());
+			}
+			
+			
+			// 5. sql 실행, executeQuery는 rs, executeUpdatesms int로 반환
+			result = (pstmt.executeUpdate() == 1);
+			
+		}	catch (SQLException e) {
+			System.out.println("error: " + e);
+		} finally {
+			// 자원 정리
+			try {
+				if (pstmt != null) {
+					pstmt.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return result;
+	}
 	
 	public boolean insert(UserVo vo) {
 		boolean result = false;
 		Connection conn = null;
 		PreparedStatement pstmt = null;
-		ResultSet rs = null;
 		
 		try {
 			conn = getConnection();
@@ -62,6 +112,57 @@ public class UserRepository {
 		} finally {
 			// 자원 정리
 			try {
+				if (pstmt != null) {
+					pstmt.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return result;
+	}
+	
+	public UserVo findByNo(Long userNo) {
+		UserVo result = null;
+		
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+			conn = getConnection();
+			
+			//3. SQL준비
+			String sql = "select no, name, email, gender from user where no=?";
+			pstmt = conn.prepareStatement(sql);
+			
+			//4. 바인딩
+			pstmt.setLong(1, userNo);
+			
+			//5. SQL실행
+			rs = pstmt.executeQuery();
+			if(rs.next()) {  //여러 개가 아니라 1개 리턴되기 때문에 while이 아닌 if를 써도된다.
+				Long no = rs.getLong(1);
+				String name = rs.getString(2);
+				String email = rs.getString(3);
+				String gender = rs.getString(4);
+//				Long no = rs.getLong(5);
+				
+				result = new UserVo();
+				result.setNo(no);
+				result.setName(name);
+				result.setEmail(email);
+				result.setGender(gender);
+			}
+		} catch (SQLException e) {
+			System.out.println("error: " + e);
+		} finally {
+			// 자원 정리
+			try {
 				if (rs != null) {
 					rs.close();
 				}
@@ -78,6 +179,7 @@ public class UserRepository {
 		
 		return result;
 	}
+	
 	
 	public UserVo findByEmailAndPassword(String email, String password) {
 		UserVo result = null;
@@ -127,119 +229,6 @@ public class UserRepository {
 		}
 		
 		return result;
-	}
-
-
-	public UserVo findByNo(Long no) {
-		UserVo result = null;
-		
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		
-		try {
-			conn = getConnection();
-			
-			//3. SQL준비
-			String sql = "select name,email,password,gender from user where no=?";
-			pstmt = conn.prepareStatement(sql);
-			
-			//4. 바인딩
-			pstmt.setLong(1, no);
-			
-			//5. SQL실행
-			rs = pstmt.executeQuery();
-			if(rs.next()) {  //여러 개가 아니라 1개 리턴되기 때문에 while이 아닌 if를 써도된다.
-				String name = rs.getString(1);
-				String email = rs.getString(2);
-				String password = rs.getString(3);
-				String gender = rs.getString(4);
-//				Long no = rs.getLong(5);
-				
-				result = new UserVo();
-				result.setName(name);
-				result.setEmail(email);
-				result.setPassword(password);
-				result.setGender(gender);
-				result.setNo(no);
-			}
-		} catch (SQLException e) {
-			System.out.println("error: " + e);
-		} finally {
-			// 자원 정리
-			try {
-				if (rs != null) {
-					rs.close();
-				}
-				if (pstmt != null) {
-					pstmt.close();
-				}
-				if (conn != null) {
-					conn.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-		
-		return result;
-	}
-	
-	public boolean updateUser(UserVo userVo) {
-		boolean result = false;
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		
-		try {
-			conn = getConnection();
-			String sql = null;
-			
-			//3. SQL준비
-			if(userVo.getPassword() == null && "".equals(userVo.getPassword())) {
-				sql = "update user set name=?, gender=? where no=?";
-			
-			}else {
-				sql = "update user set name=?, gender=?, password=? where no=?";
-			}
-			
-			pstmt = conn.prepareStatement(sql);
-			
-			// 4. 바인딩(binding)
-			pstmt.setString(1, userVo.getName());
-			pstmt.setString(2,userVo.getGender());
-			
-			if(userVo.getPassword() == null) {
-				pstmt.setLong(3, userVo.getNo());
-			}
-			else {
-				pstmt.setString(3, userVo.getPassword());
-				pstmt.setLong(4, userVo.getNo());
-			}
-			
-			// 5. sql 실행, executeQuery는 rs, executeUpdatesms int로 반환
-			result = (pstmt.executeUpdate() == 1);
-			
-		}	catch (SQLException e) {
-			System.out.println("error: " + e);
-		} finally {
-			// 자원 정리
-			try {
-				if (rs != null) {
-					rs.close();
-				}
-				if (pstmt != null) {
-					pstmt.close();
-				}
-				if (conn != null) {
-					conn.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-		return result;
-	}
-		
+	}		
 		
 }
