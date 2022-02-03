97135babe807f78f3ad5373f530aa78f64aecba7
Bella <shdudtnr3939@naver.com>
2022-02-03 20:41:51 +0900

    22.02.03 AuthInterceptor role 처리 및 @Auth target class 처리

--- mysite03/src/main/java/com/poscoict/mysite/controller/AdminController.java
+++ mysite03/src/main/java/com/poscoict/mysite/controller/AdminController.java
@@ -10,11 +10,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.poscoict.mysite.security.Auth;
 import com.poscoict.mysite.service.SiteService;
 import com.poscoict.mysite.service.UploadService;
 import com.poscoict.mysite.vo.SiteVo;
 
-//@Auth(role="ADMIN")
+@Auth(role="ADMIN")
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
--- mysite03/src/main/java/com/poscoict/mysite/security/AuthInterceptor.java
+++ mysite03/src/main/java/com/poscoict/mysite/security/AuthInterceptor.java
@@ -14,9 +14,9 @@ public class AuthInterceptor extends HandlerInterceptorAdapter {
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
-		// 1. handler 종류 확인 필요  
+		// 1. handler 종류 확인 필요 
 		if(handler instanceof HandlerMethod == false) {
-			return true;
+			return true; 	// true면 Controller에 있는 메서드가 아니므로 그대로 Controller 진행 
 		}
 		
 		// 2. casting
@@ -26,28 +26,42 @@ public class AuthInterceptor extends HandlerInterceptorAdapter {
 		// 없으면 auth에 null return
 		Auth auth = handlerMethod.getMethodAnnotation(Auth.class);
 		
+		// 4. Handler Method @Auth가 없다면 Type(Class)에 있는지 확인(과제)
+		if(auth==null) {
+			auth = handlerMethod.getBeanType().getAnnotation(Auth.class);
+		}
 		
 		// 4. @Auth가 없다면 인증 필요없다는 소리니까 return true로 cont 실행
-		// 4. Handler Method @Auth가 없다면 Type에 있는지 확인(과제)
+		// type과 method에 @Auth가 적용이 안 되어 있는 경우 
 		if(auth == null) {
 			return true;
 		}
 		
 		// 5. @Auth가 적용되어 있기 때문에 인증(Authentication) 여부 확인
-		// 5. type과 method에 @Auth가 적용이 안 되어 있는 경우 
+		// session이 있는지 체크 
 		HttpSession session = request.getSession();
 		if(session == null) {
 			response.sendRedirect(request.getContextPath()+"/user/login");
 			return false;	//인증해야하니까 cont로 돌리지 않게끔 false
 		}
 		
+		// 6. session이 존재하면 유효한 유저인지 확인 
 		UserVo authUser = (UserVo)session.getAttribute("authUser");
 		if(authUser == null) {
 			response.sendRedirect(request.getContextPath()+"/user/login");
 			return false;	//인증해야하니까 cont로 돌리지 않게끔 false;
 		}
-		// 45까지 왔다는 건 인증이 확인되었다는 소리이므로 
-		// 6. 인증 확인 완료 !! return true로 cont의 handler(method) 실행
+		// 7. admin일 경우 
+		String role = auth.role();
+		if("ADMIN".equals(role)) {
+			// admin이 아니면 메인 페이지로 돌리기 
+			if("ADMIN".equals(authUser.getRole()) == false) {
+				response.sendRedirect(request.getContextPath());
+				return false;
+			}
+		}
+		
+		// 8. 접근 허가, 인증 확인 완료 !! return true로 cont의 handler(method) 실행
 		return true;
 	}
 	
