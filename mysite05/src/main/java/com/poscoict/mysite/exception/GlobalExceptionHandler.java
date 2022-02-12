da0e486092cb72453ac7c73fb713474aa83de79b
Bella <shdudtnr3939@naver.com>
2022-02-12 15:22:10 +0900

    22.02.12 GlobalException 404 error 처리

--- mysite05/src/main/java/com/poscoict/mysite/exception/GlobalExceptionHandler.java
+++ mysite05/src/main/java/com/poscoict/mysite/exception/GlobalExceptionHandler.java
@@ -8,29 +8,35 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
 	private static final Log LOGGER = LogFactory.getLog(GlobalExceptionHandler.class);
-	
+
 	@ExceptionHandler(Exception.class)
 	public String ExceptionHandler(Model model, Exception e) {
 		// 1. loging
-		//String errors = e.toString();
-		
+		// String errors = e.toString();
+
 		StringWriter errors = new StringWriter();
 		e.printStackTrace(new PrintWriter(errors));
 		System.out.println(errors.toString());
 		LOGGER.error(errors.toString());
-		
+
 		errors.toString();
 		model.addAttribute("exception", errors.toString());
-		
+
+		// 404 Error 처리
+		if (e instanceof NoHandlerFoundException) {
+			return "error/404";
+		}
+
 		// 2. 사과 페이지 (HTML 응답, 정상 종료)
 		return "error/exception";
-		
+
 		// 3.
-		
+
 	}
 }
