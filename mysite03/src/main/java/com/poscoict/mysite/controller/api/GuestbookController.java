2425d1e056cc8769cf412ac46989f6539737d1e8
Bella <shdudtnr3939@naver.com>
2022-03-02 15:54:47 +0900

    22.03.02 guestbook spa ajax -3

---
+++ mysite03/src/main/java/com/poscoict/mysite/controller/api/GuestbookController.java
@@ -0,0 +1,11 @@
+package com.poscoict.mysite.controller.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/guestbookApiController")
+@RequestMapping("/api/guestbook")
+public class GuestbookController {
+
+	
+}
